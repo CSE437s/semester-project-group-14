6585198ff6ad279a4004e9b4936561690ab916a6
linusjlee <linuspotatoes24@gmail.com>
2024-03-01 16:17:00 -0600

    prompt moved to a popup on feed screen.

--- ReactProject/contexts/PromptContext.js
+++ ReactProject/contexts/PromptContext.js
@@ -4,13 +4,13 @@ const PromptContext = createContext();
 export default PromptContext;
 
 export const PromptProvider = ({ children }) => {
-    const [prompt, setPrompt] = useState(''); // State variable for the prompt
-    const [isPromptAnswered, setIsPromptAnswered] = useState(false); // New state variable for tracking if prompt is answered
-  
-    return (
-      <PromptContext.Provider value={{ prompt, isPromptAnswered, setPrompt, setIsPromptAnswered }}>
-        {children}
-      </PromptContext.Provider>
-    );
-  };
+  const [prompt, setPrompt] = useState(''); // State variable for the prompt
+  const [isPromptAnswered, setIsPromptAnswered] = useState(false); // New state variable for tracking if prompt is answered
+
+  return (
+    <PromptContext.Provider value={[prompt, setPrompt, isPromptAnswered, setIsPromptAnswered]}>
+      {children}
+    </PromptContext.Provider>
+  );
+};
   
