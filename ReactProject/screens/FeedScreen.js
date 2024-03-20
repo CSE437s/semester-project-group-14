a841cf4d475f8cd11e533c35f6a8efa1fa11b5a1
tar3kmp3 <elbialy.t@wustl.edu>
2024-03-20 01:26:23 -0500

    like bug fix

--- ReactProject/screens/FeedScreen.js
+++ ReactProject/screens/FeedScreen.js
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { StyleSheet, View, ActivityIndicator, Text, Image, FlatList, Modal, TextInput, TouchableOpacity, } from "react-native";
 import { Card, Button } from "tamagui";
 import PromptContext from "../contexts/PromptContext";
-import { getDocs, deleteDoc, addDoc, query, collectionGroup, where, doc, getDoc, collection } from "firebase/firestore";
+import { getDocs, deleteDoc, addDoc, query, collectionGroup, where, doc, getDoc, collection,onSnapshot } from "firebase/firestore";
 import { db, auth } from "../firebaseConfig";
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView, SafeAreaProvider, useSafeAreaInsets, } from 'react-native-safe-area-context';
@@ -20,10 +20,10 @@ export default function FeedScreen() {
   const [prompt, setPrompt, isPromptAnswered, setIsPromptAnswered] = useContext(PromptContext); // Destructuring context values
   const [newEssence, setNewEssence] = useState("");
   const insets = useSafeAreaInsets();
-
   useEffect(() => {
     const currentUserId = auth.currentUser?.uid;
     if (!currentUserId) return;
+  
     const fetchData = async () => {
       setLoading(true);
       try {
@@ -33,42 +33,50 @@ export default function FeedScreen() {
         }
   
         const followingRef = collection(db, "users", currentUserId, "following");
-        const followingSnapshot = await getDocs(followingRef);
-        const followingIds = followingSnapshot.docs.map(doc => doc.id);
+        const unsubscribeFollowing = onSnapshot(followingRef, (snapshot) => {
+          const followingIds = snapshot.docs.map(doc => doc.id);
+          console.log(followingIds);
   
-        console.log(followingIds);
-
-        const essencesRef = collectionGroup(db, "essences");
-        const q = query(essencesRef, where("prompt", "==", prompt));
-        const querySnapshot = await getDocs(q);
-        
-        const essencesDataPromises = querySnapshot.docs.map(async (essenceDoc) => {
-          const essenceData = essenceDoc.data();
-          const likesQuerySnapshot = await getDocs(collection(db, `users/${essenceData.userId}/essences/${essenceDoc.id}/likes`));
-          const numLikes = likesQuerySnapshot.size;
+          const essencesRef = collectionGroup(db, "essences");
+          const q = query(essencesRef, where("prompt", "==", prompt));
+          const unsubscribeEssences = onSnapshot(q, async (querySnapshot) => {
+            const essencesDataPromises = querySnapshot.docs.map(async (essenceDoc) => {
+              const essenceData = essenceDoc.data();
+              const likesQuerySnapshot = await getDocs(collection(db, `users/${essenceData.userId}/essences/${essenceDoc.id}/likes`));
+              const numLikes = likesQuerySnapshot.size;
   
-          let username = "USER"; // Default username
-          if (essenceData.userId) {
-            const userDocRef = doc(db, "users", essenceData.userId);
-            const userDoc = await getDoc(userDocRef);
-            username = userDoc.exists() ? userDoc.data().username || "USER" : "Unknown User";
-          }
+              let username = "USER"; // Default username
+              if (essenceData.userId) {
+                const userDocRef = doc(db, "users", essenceData.userId);
+                const userDoc = await getDoc(userDocRef);
+                username = userDoc.exists() ? userDoc.data().username || "USER" : "Unknown User";
+              }
   
-          if (followingIds.includes(essenceData.userId)) {
-            return {
-              id: essenceDoc.id,
-              ...essenceData,
-              username,
-              numLikes,
-            };
-          } else {
-            return null; // If not, return null
-          }
+              if (followingIds.includes(essenceData.userId)) {
+                return {
+                  id: essenceDoc.id,
+                  ...essenceData,
+                  username,
+                  numLikes,
+                };
+              } else {
+                return null; // If not, return null
+              }
+            });
+  
+            const essencesWithData = await Promise.all(essencesDataPromises);
+            const filteredEssences = essencesWithData.filter(essence => essence !== null);
+            setFeedData(filteredEssences);
+          });
+  
+          return () => {
+            unsubscribeEssences();
+          };
         });
   
-        const essencesWithData = await Promise.all(essencesDataPromises);
-        const filteredEssences = essencesWithData.filter(essence => essence !== null);
-        setFeedData(filteredEssences);
+        return () => {
+          unsubscribeFollowing();
+        };
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
@@ -78,7 +86,6 @@ export default function FeedScreen() {
   
     fetchData();
   }, [prompt]);
-  
 
   useEffect(() => { // Set navigation options in this useEffect
     navigation.setOptions({
@@ -151,25 +158,20 @@ export default function FeedScreen() {
   };
 
   const handleLike = async (essenceId, postUserId) => {
-    console.log(postUserId);
     const userId = auth.currentUser?.uid;
     const likesRef = collection(db, `users/${postUserId}/essences/${essenceId}/likes`);
     const querySnapshot = await getDocs(
       query(likesRef, where("userId", "==", userId))
     );
-    const likeData = {
-      userId: userId,
-      likedAt: new Date(),
-    };
   
     if (!querySnapshot.empty) {
       querySnapshot.forEach(async (doc) => {
         try {
           await deleteDoc(doc.ref);
-          console.log("remove like");
+          console.log("removed like");
         } catch (error) {
-          console.error("Error unliking essence: ", error);
-          alert("An error occurred while unliking essence. Please try again.");
+          console.error("Error removing like: ", error);
+          alert("An error occurred while removing like. Please try again.");
         }
       });
     } else {
@@ -179,26 +181,30 @@ export default function FeedScreen() {
       };
   
       try {
-        await addDoc(collection(db, `users/${postUserId}/essences/${essenceId}/likes`), likeData);
+        await addDoc(likesRef, likeData);
         console.log("added like");
       } catch (error) {
-        console.error("Error liking essence: ", error);
-        alert("An error occurred while liking essence. Please try again.");
+        console.error("Error adding like: ", error);
+        alert("An error occurred while adding like. Please try again.");
       }
     }
   
-    // Update the feedData directly with the updated numLikes
+    const updatedLikesSnapshot = await getDocs(likesRef);
+    const updatedNumLikes = updatedLikesSnapshot.size;
+  
     const updatedFeedData = feedData.map(essence => {
       if (essence.id === essenceId) {
         return {
           ...essence,
-          numLikes: querySnapshot.empty ? essence.numLikes - 1 : essence.numLikes + 1,
+          numLikes: updatedNumLikes,
         };
       }
       return essence;
     });
+  
     setFeedData(updatedFeedData);
   };
+  
 
   const renderItem = ({ item }) => (
     <View style={styles.card}>
