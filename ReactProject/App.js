e97d5b6235ee3837542a159c46aa0ec90bd16eca
linusjlee <linuspotatoes24@gmail.com>
2024-04-16 11:27:57 -0500

    somehow got expo go to work on phone now.

--- ReactProject/App.js
+++ ReactProject/App.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, createContext } from "react";
-import { StyleSheet,View,Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import React, { useState, useEffect, createContext, useRef } from "react";
+import { StyleSheet,Button, View,Text, Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import "@tamagui/core/reset.css";
 import { TamaguiProvider } from "tamagui";
 import tamaguiConfig from "./tamagui.config";
@@ -17,6 +17,88 @@ import { collection,addDoc, deleteDoc,getDocs } from "firebase/firestore";
 import PromptContext from "./contexts/PromptContext";
 import ProfileScreen from "./screens/ProfileScreen";
 
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+async function sendPushNotification(expoPushToken) {
+  const message = {
+    to: expoPushToken,
+    sound: 'default',
+    title: 'Original Title',
+    body: 'And here is the body!',
+    data: { someData: 'goes here' },
+  };
+
+  await fetch('https://exp.host/--/api/v2/push/send', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Accept-encoding': 'gzip, deflate',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(message),
+  });
+}
+
+function handleRegistrationError(errorMessage) {
+  alert(errorMessage);
+  throw new Error(errorMessage);
+}
+
+async function registerForPushNotificationsAsync() {
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      handleRegistrationError('Permission not granted to get push token for push notification!');
+      return;
+    }
+    const projectId =
+      Constants?.expoConfig?.extra?.eas?.projectId ??
+      Constants?.easConfig?.projectId;
+    if (!projectId) {
+      handleRegistrationError('Project ID not found');
+    }
+    try {
+    const pushTokenString = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+      console.log(pushTokenString);
+      return pushTokenString;
+    } catch (e) {
+      handleRegistrationError(`${e}`);
+    }
+  } else {
+    handleRegistrationError('Must use physical device for push notifications');
+  }
+}
+
+
 
 // const PromptContext = createContext();
 const Stack = createStackNavigator();
@@ -26,6 +108,36 @@ export default function App() {
   const [prompt, setPrompt] = useState("");
   const [isPromptAnswered, setIsPromptAnswered] = useState(false);
   const [countdown, setCountdown] = useState("");
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(undefined);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ''))
+      .catch((error) => setExpoPushToken(`${error}`));
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(
+          notificationListener.current,
+        );
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -34,6 +146,8 @@ export default function App() {
 
     return unsubscribe;
   }, []);
+
+  
 useEffect(() => {
     let isMounted = true;
 
@@ -49,6 +163,7 @@ useEffect(() => {
         } else {
             console.error("No prompts found in the database.");
         }
+        return;
     }
   
       let topPrompt = null;
@@ -120,10 +235,10 @@ useEffect(() => {
 
   return (
     <PromptContext.Provider value={ [prompt, setPrompt, isPromptAnswered, setIsPromptAnswered] }>
-      <TamaguiProvider config={tamaguiConfig}>
-        <NavigationContainer>
-          <Stack.Navigator>
-            {user ? (
+       <TamaguiProvider config={tamaguiConfig}>
+         <NavigationContainer>
+           <Stack.Navigator>
+             {user ? (
               <>
                 <Stack.Screen name="Main" component={FooterNavigator} options={{ headerShown: false }} />
                 <Stack.Screen name="Follow" component={FollowScreen}  />
@@ -141,6 +256,23 @@ useEffect(() => {
 
             )}
           </Stack.Navigator>
+          <Text>Your Expo push token: {expoPushToken}</Text>
+            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            {notification && notification.request && notification.request.content && (
+            <>
+                <Text>Title: {notification.request.content.title}</Text>
+                <Text>Body: {notification.request.content.body}</Text>
+                <Text>Data: {JSON.stringify(notification.request.content.data)}</Text>
+              </>
+            )}
+            </View>
+            <Button
+              title="Press to Send Notification"
+              onPress={async () => {
+                await sendPushNotification(expoPushToken);
+              }}
+            />
+
         </NavigationContainer>
       </TamaguiProvider>
     </PromptContext.Provider>
@@ -156,3 +288,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+
