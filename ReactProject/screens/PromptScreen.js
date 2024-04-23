86b70922b0544db10e4d4f43a0943cd0ae634d93
Ethan Leifert <e.leifert@wustl.edu>
2024-04-23 13:06:21 -0500

    upvote/downvote button fix

--- ReactProject/screens/PromptScreen.js
+++ ReactProject/screens/PromptScreen.js
@@ -311,13 +311,13 @@ const PromptScreen = ({ navigation }) => {
     <View style={styles.bottomContainer}>
       <View style={styles.voteContainer}>
         <View style={styles.voteSection}>
-          <TouchableOpacity onPress={() => handleVote(prompt.id, "upvote")} >
+          <TouchableOpacity style={styles.voteButton} onPress={() => handleVote(prompt.id, "upvote")} >
             <Ionicons name="caret-up" size={18} color="#3B82F6" />
           </TouchableOpacity>
           <Text style={styles.voteCount}>{prompt.upvotes.length}</Text>
         </View>
         <View style={styles.voteSection}>
-          <TouchableOpacity onPress={() => handleVote(prompt.id, "downvote")}>
+          <TouchableOpacity style={styles.voteButton} onPress={() => handleVote(prompt.id, "downvote")}>
             <Ionicons name="caret-down" size={18} color="#FF6347" />
           </TouchableOpacity>
           <Text style={styles.voteCount}>{prompt.downvotes.length}</Text>
@@ -458,6 +458,14 @@ const styles = StyleSheet.create({
   },
   voteCount: {
     marginLeft: 5,
+  },
+  voteButton: {
+    backgroundColor: '#F0F0F0', // Set a background color for the vote buttons
+    borderRadius: 5,
+    padding: 3, // Reduce padding for the buttons
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 5, // Add some small margin between the buttons
   },
   input: {
     backgroundColor: '#F5F5F5',
